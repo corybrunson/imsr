c40b661fdbf2099a6ac244321466b24d7016d495
Jason Cory Brunson <jason.brunson@medicine.ufl.edu>
2023-08-15 14:58:46 -0400

    continue transcribing straightforward responses to reviewers

--- scr/synthesis.r
+++ scr/synthesis.r
@@ -1,4 +1,69 @@
 library(tidyverse)
+library(ggmosaic)
+
+# scrape and format table summarizing studies included in synthesis
+# Excel file downloaded from Google Sheet:
+# https://docs.google.com/spreadsheets/d/
+# 1tpWMhYH2pyRT55K7n2J2XFs-kEV_JTuCDmXzJ4BBgNo/
+googlesheets4::read_sheet(
+  ss = "1tpWMhYH2pyRT55K7n2J2XFs-kEV_JTuCDmXzJ4BBgNo", sheet = 1L
+) |>
+  rename_with(snakecase::to_snake_case) |>
+  filter(is.na(synthesis)) |> 
+  arrange(date_of_publication) |> 
+  transmute(
+    Citation = paste0("@", zotero_key),
+    Date = format(date_of_publication, "%Y %b %d"),
+    Task = objective,
+    Aim = ifelse(generalizable_knowledge == "Science", "Knowledge", "Practice"),
+    Source = type_of_data,
+    Type = range_of_data,
+    `Cases` = number_of_cases_incidents,
+    `Features` = number_of_predictors_features
+  ) |>
+  mutate(Task = ifelse(Task == "Recommendation", "Decision Support", Task)) |> 
+  mutate(across(c(Cases, Features), sapply, FUN = format, big.mark = ",")) |>
+  mutate(across(c(Cases, Features), sapply, FUN = paste0, collapse = "; ")) |>
+  # mutate(across(c(Cases, Features), str_split, pattern = "; ")) |> 
+  # mutate(Dimensions = purrr::map2_chr(
+  #   Cases, Features,
+  #   ~ str_c(.x, .y, sep = " × ", collapse = "; ")
+  # )) |> 
+  mutate(across(
+    everything(),
+    # ~ stringr::str_replace_all(., "<U+001B>", "")
+    textclean::replace_non_ascii
+  )) ->
+  tab_synthesis
+# write to file, to be read into document
+write_rds(tab_synthesis, file = here::here("tab/synthesis.rds"))
+
+# mosaic plot of task, aim, and source
+tab_synthesis %>%
+  mutate(Source = factor(
+    Source,
+    c("Clinical", "Laboratory", "Imaging",
+      "Clinical, Laboratory", "Clinical, Imaging", "Patient-reported")
+  )) %>%
+  mutate(Task = factor(
+    Task,
+    c("Prediction", "Clustering", "Diagnosis", "Prognosis", "Decision Support")
+  )) %>%
+  mutate(Aim = factor(Aim, c("Knowledge", "Practice"))) %>%
+  # mutate(across(c(Source, Task, Aim), fct_infreq)) %>%
+  count(Source, Task, Aim) %>%
+  ggplot() +
+  theme_bw() +
+  theme(panel.grid = element_blank()) +
+  geom_mosaic(
+    aes(x = product(Task, Source), fill = Task, weight = n),
+    # aes(x = product(Task, Source), fill = Task, alpha = Aim, weight = n),
+    divider = mosaic("v")
+  ) +
+  # coord_flip() +
+  scale_y_productlist("Source", labels = ggmosaic:::product_labels()) +
+  scale_fill_brewer(type = "qual", na.value = "#000000") +
+  scale_alpha_manual(values = c(Knowledge = .5, Practice = 1))
 
 # Excel file downloaded from Google Sheet:
 # https://docs.google.com/spreadsheets/d/
@@ -8,7 +73,8 @@ library(tidyverse)
 googlesheets4::read_sheet(
   ss = "1xvDJwiLBoI2oz8fxHJ5MjNmiju_RAlK7RJv-wXe1DAs", sheet = 1L
 ) |> 
-  select(seq(2L, 4L)) %>%
+  filter(is.na(Synthesis)) %>%
+  select(seq(3L, 5L)) %>%
   rename_with(~ str_replace(snakecase::to_snake_case(.), "_s$", "s")) %>%
   mutate(across(c(approach_types, terms), ~ str_split(., "(;|,) *"))) %>%
   print() -> composite_studies
@@ -26,6 +92,7 @@ composite_studies %>%
   ) +
   labs(x = NULL, y = NULL) ->
   year_plot
+print(year_plot)
 ggsave(here::here("fig/fig-years.png"), year_plot, width = 5, height = 2)
 
 # approach types
@@ -76,3 +143,4 @@ composite_terms %>%
   arrange(desc(n), terms) %>%
   print(n = Inf)
 
+# 
